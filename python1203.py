c1feee19d3d5042fff0f6c0ae57d1dc81610a556
bruh00000 <lookpee0911@gmail.com>
2024-08-05 15:13:44 +0700

    first commit

---
+++ python1203.py
@@ -0,0 +1,39 @@
+#3.Comparison Operator
+a = 10
+b = 12
+c = 5
+d = 2
+
+#3.1.Output=>: a > b is fasle
+print('a > b is ',a>b)
+
+#3.2.Output=>: a < b is true
+print('a < b is ',a<b)
+
+#3.3.Output=>: a == b is fasle
+print('a == b is ',a==b)
+
+#3.4.Output=>: a != b is true
+print('a != b is ',a!=b)
+
+#3.5.Output=>: a >= b is fasle
+print('a >= b is ',a>=b)
+
+#3.6.Output=>: a >= b is true
+print('a <= b is ',a<=b)
+
+#3.7.Equal to operator
+print('c = d is ',c=d)
+
+#3.8. not Equal to operator
+print('c != d is ',c!=d)
+
+#3.9.Greater than operator
+print('c > d is ',c>d)
+
+#3.10.Less than operator
+print('c < d is ',c<d)
+
+#3.11.Greater or equal to operator
+print('c >= d is ',c>=d)
+
---
+++ python1204.py
@@ -0,0 +1,12 @@
+#4. logic Operator
+a = True
+b = False
+
+#4.1 Logical and 
+print("Result : ", a & b) #=>: Fasle
+
+#4.2 Logical or
+print("Result : ", a | b) #=>: True
+
+#4.3 Logical not
+print("Result : ", (not a)) #=>: Fasle
